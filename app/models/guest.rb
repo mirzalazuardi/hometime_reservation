a39d02120992ce0dd4e052d1c80ff456cf59bd5d
Mirzalazuardi H <mirzalazuardi@gmail.com>
2022-06-08 12:49:04 +0700

    add uniq validation

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,2 +1,3 @@
 class Guest < ApplicationRecord
+  validates_uniqueness_of :email
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,4 @@
 class Reservation < ApplicationRecord
   belongs_to :guest
+  validates_uniqueness_of :code
 end
