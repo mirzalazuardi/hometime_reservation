0d00985c1e3b5eea4428d6f7174ca533b309d7d8
Mirzalazuardi H <mirzalazuardi@gmail.com>
2022-06-09 01:06:31 +0700

    change update url format on route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace 'api', defaults: {format: :json} do
     namespace 'v1' do
-      resources :reservations
+      resources :reservations, except: [:new, :edit, :update]
+      put 'reservations', to: 'reservations#update'
+      patch 'reservations', to: 'reservations#update'
     end
   end
 end
