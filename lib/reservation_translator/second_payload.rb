5538b60a20da0fd1fab4df3256b8c38129e9dea9
Mirzalazuardi H <mirzalazuardi@gmail.com>
2022-06-08 18:41:52 +0700

    comment unused

--- lib/reservation_translator/second_payload.rb
+++ lib/reservation_translator/second_payload.rb
@@ -101,29 +101,29 @@ module ReservationTranslator
   end
 end
 
-hash = {
-"reservation": {
-  "code": "XXX12345678",
-  "start_date": "2021-03-12",
-  "end_date": "2021-03-16",
-  "expected_payout_amount": "3800.00",
-  "guest_details": {
-  "localized_description": "4 guests",
-  "number_of_adults": 2,
-  "number_of_children": 2,
-  "number_of_infants": 0
-  },
-  "guest_email": "wayne_woodbridge@bnb.com",
-  "guest_first_name": "Wayne",
-  "guest_last_name": "Woodbridge",
-  "guest_phone_numbers": [ "639123456789", "639123456789" ],
-  "listing_security_price_accurate": "500.00",
-  "host_currency": "AUD",
-  "nights": 4,
-  "number_of_guests": 4,
-  "status_type": "accepted",
-  "total_paid_amount_accurate": "4300.00"
-  }
-}
+#hash = {
+#"reservation": {
+  #"code": "XXX12345678",
+  #"start_date": "2021-03-12",
+  #"end_date": "2021-03-16",
+  #"expected_payout_amount": "3800.00",
+  #"guest_details": {
+  #"localized_description": "4 guests",
+  #"number_of_adults": 2,
+  #"number_of_children": 2,
+  #"number_of_infants": 0
+  #},
+  #"guest_email": "wayne_woodbridge@bnb.com",
+  #"guest_first_name": "Wayne",
+  #"guest_last_name": "Woodbridge",
+  #"guest_phone_numbers": [ "639123456789", "639123456789" ],
+  #"listing_security_price_accurate": "500.00",
+  #"host_currency": "AUD",
+  #"nights": 4,
+  #"number_of_guests": 4,
+  #"status_type": "accepted",
+  #"total_paid_amount_accurate": "4300.00"
+  #}
+#}
 
-puts ReservationTranslator::SecondPayload.new(hash).call.inspect
+#puts ReservationTranslator::SecondPayload.new(hash).call.inspect
