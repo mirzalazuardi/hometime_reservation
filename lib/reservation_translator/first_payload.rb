43e35fb54779d8a87a7875413e2248a5da80b0c0
Mirzalazuardi H <mirzalazuardi@gmail.com>
2022-06-08 17:46:04 +0700

    add class method sorted all keys

--- lib/reservation_translator/first_payload.rb
+++ lib/reservation_translator/first_payload.rb
@@ -5,7 +5,7 @@ class ReservationTranslator::FirstPayload
     @attrs = reservation_hash.with_indifferent_access
   end
 
-  def self.sorted_keys
+  def self.sorted_all_keys
     arr = %w(reservation_code start_date end_date nights)
     arr << %w(guests adults children infants status)
     arr << %w(guest currency payout_price security_price)
@@ -13,12 +13,11 @@ class ReservationTranslator::FirstPayload
     arr.sort
   end
 
-  def self.sorted_subkeys
-    [
-      {
-        guest: %w(first_name last_name phone email).sort
-      }.with_indifferent_access
-    ]
+  def self.sorted_accepted_keys
+    sorted_all_keys
+  end
+
+  def self.sorted_accepted_subkeys
   end
 
   def call
@@ -40,29 +39,54 @@ class ReservationTranslator::FirstPayload
   end
 
   def adults_amount
+    attrs[:adults].to_i
   end
+
   def children_amount
+    attrs[:children].to_i
   end
+
   def code
+    attrs[:reservation_code]
   end
+
   def currency
+    attrs[:currency].upcase
   end
+
   def end_date
+    attrs[:end_date].to_date
   end
+
   def infants_amount
+    attrs[:infants].to_i
   end
+
   def nights_quota
+    attrs[:nights].to_i
   end
+
   def payout_price
+    attrs[:payout_price].to_f
   end
+
   def security_price
+    attrs[:security_price].to_f
   end
+
   def start_date
+    attrs[:start_date].to_date
   end
+
   def status
+    attrs[:status]
   end
+
   def total_price
+    (payout_price + security_price).to_f
   end
+
   def guest
+    GuestTranslator.new(attrs).call
   end
 end
