d7544a88aa052b6280cb5c054e6a577852fc39be
Mirzalazuardi H <mirzalazuardi@gmail.com>
2022-06-08 17:07:43 +0700

    add lib for guest translator - second payload

---
+++ lib/guest_translator/second_payload.rb
@@ -0,0 +1,41 @@
+class GuestTranslator::SecondPayload
+  attr_reader :attrs
+
+  def initialize(reservation_hash)
+    @attrs = reservation_hash.with_indifferent_access
+  end
+
+  def self.sorted_keys
+    arr = %w(guest_details guest_email guest_first_name)
+    arr << %w(guest_last_name guest_phone_numbers)
+    arr.sort
+  end
+
+  def self.sorted_subkeys
+  end
+
+  def call
+    {
+      email: email,
+      first_name: first_name,
+      last_name: last_name,
+      guest_phones_attributes: guest_phones
+    }
+  end
+
+  def email
+    attrs[:guest_email]
+  end
+
+  def first_name
+    attrs[:guest_first_name]
+  end
+
+  def last_name
+    attrs[:guest_last_name]
+  end
+
+  def guest_phones
+    attrs[:guest_phone_numbers]
+  end
+end
