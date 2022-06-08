5a0e03b8d8859e7141ef25844b71165e350d6fdd
Mirzalazuardi H <mirzalazuardi@gmail.com>
2022-06-09 01:05:57 +0700

    add method for cut nested key

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::API
   include ActionController::MimeResponds
+
+  def except_nested_key(hash_obj, keys)
+     Hash[hash_obj.map {|k,v| [k,(v.respond_to?(:except)?v.except(*keys):v)] }]
+  end
 end
