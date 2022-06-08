75be90b34cc3ef3da729fccbbdb6d56617c083ac
Mirzalazuardi H <mirzalazuardi@gmail.com>
2022-06-08 16:56:53 +0700

    fix lib guest translator firstpayload

--- lib/guest_translator/first_payload.rb
+++ lib/guest_translator/first_payload.rb
@@ -1,12 +1,20 @@
 class GuestTranslator::FirstPayload
   attr_reader :attrs
 
-  def initialize(guest_hash)
+  def initialize(reservation_hash)
     @attrs = reservation_hash.with_indifferent_access
   end
 
   def self.sorted_keys
-    %w(first_name last_name phone email).sort
+    %w(guest)
+  end
+
+  def self.sorted_subkeys
+    [
+      {
+        guest: %w(first_name last_name phone email).sort
+      }.with_indifferent_access
+    ]
   end
 
   def call
