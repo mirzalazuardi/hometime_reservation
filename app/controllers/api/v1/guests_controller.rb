f14e31fcc31196bf5046f2aa10c52fa8c8c92307
Mirzalazuardi H <mirzalazuardi@gmail.com>
2022-06-08 14:09:02 +0700

    add methods to guest controllers

--- app/controllers/api/v1/guests_controller.rb
+++ app/controllers/api/v1/guests_controller.rb
@@ -1,2 +1,43 @@
 class Api::V1::GuestsController < ApplicationController
+  before_action :find_guest, only: [:show, :update, :destroy]
+  respond_to :json
+
+  def index
+    @guests = Guest.all
+
+    respond_with @guests
+  end
+
+  def show
+    respond_with @guest
+  end
+
+  def create
+    @guest = Guest.new(guest_params)
+    @guest.save
+
+    respond_with @guest
+  end
+
+  def update
+    @guest.update(guest_params)
+
+    respond_with @guest
+  end
+
+  def destroy
+    @guest.destroy
+
+    respond_with @guest
+  end
+
+  private
+    def find_guest
+      @guest = Guest.find(params[:id])
+    end
+
+    def guest_params
+      params.require(:guest)
+        .permit(:email, :first_name, :last_name)
+    end
 end
