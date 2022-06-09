8dec3d88584faba1c9dae7802448a3a28649999a
Mirzalazuardi H <mirzalazuardi@gmail.com>
2022-06-09 21:51:17 +0700

    add guest_phones_attributes to serializer

--- app/serializers/reservation_serializer.rb
+++ app/serializers/reservation_serializer.rb
@@ -7,4 +7,8 @@ class ReservationSerializer
   attribute :guest_attributes do |object, params|
     params[:guest_columns]&.size&.positive? ? object.guest.slice(*params[:guest_columns]) : object.guest
   end
+
+  attribute :guest_phones_attributes do |object|
+    object.guest.guest_phones.select(:id, :number)
+  end
 end
